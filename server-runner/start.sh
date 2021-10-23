39e8801d8d940537f52326b632fdca6244aaddf6
Rayvhiz <47751236+Rayvhiz@users.noreply.github.com>
2021-10-23 13:08:32 +0200

    Remove echo for debug

--- server-runner/start.sh
+++ server-runner/start.sh
@@ -3,8 +3,6 @@
 path="$(dirname "$PWD")"
 dir="$(basename "$path")"
 
-echo $path
-
 # Check if Minecraft server is already running
 if screen -list | grep -q "$dir"; then
 	echo "Minecraft server '$dir' is already running."
